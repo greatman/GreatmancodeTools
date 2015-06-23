2f9f27bcef480feb83267bd2bd42b3c995c21e51
William Lebel <lebelwilliam@gmail.com>
2015-06-22 23:40:24 -0400

    Make the join event run as early as possible, should prevent economy issues.

--- src/main/java/com/greatmancode/tools/events/bukkit/PlayerJoinEventListener.java
+++ src/main/java/com/greatmancode/tools/events/bukkit/PlayerJoinEventListener.java
@@ -21,11 +21,12 @@ package com.greatmancode.tools.events.bukkit;
 import com.greatmancode.tools.entities.Player;
 import com.greatmancode.tools.events.EventManager;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class PlayerJoinEventListener implements Listener {
-    @EventHandler
+    @EventHandler(priority = EventPriority.LOWEST)
     public void onPlayerJoin(PlayerJoinEvent event) {
         com.greatmancode.tools.events.playerEvent.PlayerJoinEvent pEvent = new com.greatmancode.tools.events.playerEvent.PlayerJoinEvent(new Player(event.getPlayer().getName(), event.getPlayer().getDisplayName(), event.getPlayer().getWorld().getName(), event.getPlayer().getUniqueId()));
         EventManager.getInstance().callEvent(pEvent);
